1e533e9defab74141836bf01c6fe960e2fc59d08
Cristina Lugo <cmlugoce@syr.edu>
2018-06-07 23:37:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    puts "Fizz" # Go fizz
+    print "Fizz" # Go fizz
     elsif int % 5 == 0 
     puts "Buzz"
     elsif int % 15 == 0
